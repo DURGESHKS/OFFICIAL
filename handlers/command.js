b9aa138fee2df80d344397b99318b1b23a743f4e
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-03-01 19:33:46 +0530

    Add files via upload

--- handlers/command.js
+++ handlers/command.js
@@ -1,14 +1,20 @@
-const { readdirSync } = require("fs");
-module.exports = (bot) => {
-    readdirSync("./commands/").map(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
-            let pull = require(`../commands/${dir}/${cmd}`)
-            console.log(`Loaded command ${pull.name}`)
-            bot.commands.set(pull.name,pull)
-            // didnt't make handler
-            if(pull.aliases){
-                pull.aliases.map(p=>bot.aliases.set(p,pull))
-            }
-        })
-    })
+const {readdirSync} = require('fs');
+const ascii = require('ascii-table')
+let table = new ascii("Commands");
+table.setHeading('Command', ' Load status');
+module.exports= (client) => {
+    readdirSync('./commands/').forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+        for(let file of commands){
+            let pull = require(`../commands/${dir}/${file}`);
+            if(pull.name){
+                client.commands.set(pull.name, pull);
+                table.addRow(file,'✅')
+            } else {
+                table.addRow(file, '❌ -> Missing a help.name, or help.name is not a string.')
+                continue;
+            }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+        }
+    });
+    console.log(table.toString());
 }
