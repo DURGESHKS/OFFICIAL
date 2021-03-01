dbc1e9ae656cbb758e48261274450391008ff2a1
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-03-01 19:34:38 +0530

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -11,7 +11,7 @@ module.exports= (client) => {
                 client.commands.set(pull.name, pull);
                 table.addRow(file,'✅')
             } else {
-                table.addRow(file, '❌ -> Missing a help.name, or help.name is not a string.')
+                table.addRow(file, '❎ -> Missing a help.name, or help.name is not a string.')
                 continue;
             }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
         }
