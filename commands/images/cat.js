0fc78a4e4bbc84f03bb7a6c7cb09cf62a72ee660
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-03-02 22:00:26 +0530

    Update cat.js

--- commands/images/cat.js
+++ commands/images/cat.js
@@ -5,6 +5,7 @@ module.exports = {
     name: "cat",
     category: "images",
     permissions: ["SEND_MESSAGES"],
+    timeout: 60000,
     run: async (client, message, args) => {
         const url = "https://some-random-api.ml/img/cat";
         const facts = "https://some-random-api.ml/facts/cat"
