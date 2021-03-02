695d994ee6c41da0bed66bf2902787575b2604d6
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-03-02 17:09:05 +0530

    Update dog.js

--- commands/images/dog.js
+++ commands/images/dog.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "image-dog",
+    name: "dog",
     category: "images",
     permissions: ["SEND_MESSAGES"],
     run: async (client, message, args) => {
