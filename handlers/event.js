b3b132be2c4bf03e1d6be470848d08552ceaabd9
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-03-04 16:04:06 +0530

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,12 +1,39 @@
-const { readdirSync } = require("fs");
-module.exports = (client) => {
-  const load = dirs => {
-    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith("js") );
-    for (let file of events) {
-      let evt = require(`../events/${dirs}/${file}`);
-      let eName = file.split('.')[0];
-      client.on(eName, evt.bind(null,client));
-    }
-  };
-  ["client", "guild"].forEach((x) => load(x));
-};
+const {readdirSync} = require('fs');
+
+const ascii = require('ascii-table')
+
+let table = new ascii("Commands");
+
+table.setHeading('Command', ' Load status');
+
+module.exports= (client) => {
+
+    readdirSync('./commands/').forEach(dir => {
+
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+
+        for(let file of commands){
+
+            let pull = require(`../commands/${dir}/${file}`);
+
+            if(pull.name){
+
+                client.commands.set(pull.name, pull);
+
+                table.addRow(file,'✅')
+
+            } else {
+
+                table.addRow(file, '❌ -> Missing a help.name, or help.name is not a string.')
+
+                continue;
+
+            }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+
+        }
+
+    });
+
+    console.log(table.toString());
+
+}
